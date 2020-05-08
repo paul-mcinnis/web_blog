8f0bf809a1c5e7b9991672e72f11d052ec19543c
Paul McInnis <mcinnis.paul@gmail.com>
2020-05-08 08:53:11 -0700

    updated file structure

---
+++ ng-app/src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
