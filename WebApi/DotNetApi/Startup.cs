00846ca28a953089cbbd7e8a37c72839e97b04e9
Paul McInnis <mcinnis.paul@gmail.com>
2020-05-20 12:38:44 -0700

    added lamar dependecy injection

--- WebApi/DotNetApi/Startup.cs
+++ WebApi/DotNetApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Lamar;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace DotNetApi
 {
@@ -21,6 +15,21 @@ namespace DotNetApi
         }
 
         public IConfiguration Configuration { get; }
+
+        public void ConfigureContainer(ServiceRegistry services)
+        {
+            // Supports ASP.Net Core DI abstractions
+            services.AddMvc();
+            services.AddLogging();
+
+            // Also exposes Lamar specific registrations
+            // and functionality
+            services.Scan(s =>
+            {
+                s.TheCallingAssembly();
+                s.WithDefaultConventions();
+            });
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
